0cb29151fb1081aca3619327c166758d337972c5
Michael Keyes <michael.d.keyes87@gmail.com>
2020-07-10 20:43:53 -0400

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Go workout",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Do laundry",
+          completed: false,
+        },
+        {
+          id: 3,
+          title: "Cook food",
+          completed: false,
+        },
+        {
+          id: 4,
+          title: "Clean up room",
+          completed: false,
+        },
+        {
+          id: 5,
+          title: "Finish work",
+          completed: false,
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  naem: "Todo",
+};
+</script>
+
+<style scoped></style>
